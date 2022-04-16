9c1c3c595d8e93e3bd3622154448baca58d63414
Massot Calvin <calvin.massot@orange.fr>
2022-04-16 11:03:20 +0200

    ajout d’un test sur une liste à 3 éléments non triés

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
         var n = input.length;
         var divider = (max + 1) / n;
         var buckets = new double[10][n];
+        var result = new double[n];
         var bucketsMarker = new int[10];
         Arrays.fill(bucketsMarker, 0);
 
@@ -18,6 +19,31 @@ public class Main {
             buckets[j][bucketsMarker[j]] = input[i];
             bucketsMarker[j] += 1;
         }
-        return input;
+        var resultCounter = 0;
+        for (int i = 0; i < buckets.length; i++) {
+            innerSort(buckets[i], bucketsMarker[i]);
+            for (int j = 0; j < bucketsMarker[i]; j++) {
+                if (resultCounter < result.length) {
+                    result[resultCounter++] = buckets[i][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    private static void innerSort(double[] array, int nbItems) {
+        var sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < nbItems - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    var tmp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = tmp;
+                    sorted = false;
+                }
+            }
+        }
     }
 }
+
