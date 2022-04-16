cd33c5091febdfa73086a84be47083ec46ecce37
Massot Calvin <calvin.massot@orange.fr>
2022-04-16 10:31:19 +0200

    fonction vide et test sur une liste vide

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
+    public static double[] sort(double[] input) {
+        var maxOpt = Arrays.stream(input).max();
+        if (maxOpt.isEmpty()) {
+            return input;
+        }
+        return input;
+    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
+    @Test
+    public void testSortEmpty() {
+        var expected = new double[0];
+        var input = new double[0];
+        Assertions.assertArrayEquals(expected, Main.sort(input));
+    }
 }
