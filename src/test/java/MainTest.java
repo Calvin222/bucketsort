a5f357f53179abf631c1fd2087a14bed6311a2be
Massot Calvin <calvin.massot@orange.fr>
2022-04-16 10:33:02 +0200

    ajout d’un test sur une liste à 1 élément

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,4 +8,12 @@ public class MainTest {
         var input = new double[0];
         Assertions.assertArrayEquals(expected, Main.sort(input));
     }
+
+    @Test
+    public void testSort1Item() {
+        var expected = new double[]{1.9f};
+        var input = new double[]{1.9f};
+        Assertions.assertArrayEquals(expected, Main.sort(input));
+    }
+
 }
