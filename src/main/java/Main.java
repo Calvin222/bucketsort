2f94824d03237f6894809d377daea73c5bb41264
Massot Calvin <calvin.massot@orange.fr>
2022-04-16 10:45:43 +0200

    ajout d’un test sur une liste à 2 éléments non triés

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,18 @@ public class Main {
         if (maxOpt.isEmpty()) {
             return input;
         }
+        var max = maxOpt.getAsDouble();
+        var n = input.length;
+        var divider = (max + 1) / n;
+        var buckets = new double[10][n];
+        var bucketsMarker = new int[10];
+        Arrays.fill(bucketsMarker, 0);
+
+        for (int i = 0; i < input.length; i++) {
+            var j = (Integer) Math.round((float) (input[i] / divider));
+            buckets[j][bucketsMarker[j]] = input[i];
+            bucketsMarker[j] += 1;
+        }
         return input;
     }
 }
